d40491cf11caa02555e1e41ecc203e89dd48b89b
tmgbedu@gmail.com <tmgbedu@gmail.com>
2018-06-03 08:26:23 +0545

    Home page design message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app-front')
 
-
 @section('content')
+<div class="container">
+    <div class="row mt-1">
+        <div class="col-md-8 pr-md-0">
+            <div class="bg-white">
+                <div class="card">
+                    <div class="card-header"><strong>Featured Product</strong></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 pl-md-1">
+            <div class="bg-white">
+                <div class="card">
+                    <div class="card-header"><strong>Top Shop</strong></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
