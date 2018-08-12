4af2dd15cd3450c6a5c9cda7c90dae44d6cd6b02
Bedus Creation <tmgbedu@gmail.com>
2018-08-13 02:29:36 +0545

    Dropzone with multiple product added

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .copy('public/js/app.js', 'public_html/app.js')
+   .copy('public/js/app.js', 'public_html/js/app.js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .copy('public/css/app.css','public_html/css/app.css');
