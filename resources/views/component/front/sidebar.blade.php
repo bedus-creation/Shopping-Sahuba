3dee39edeb47c8ad21266eb125c333df8b6cc945
bedus-creation <tmgbedu@gmail.com>
2018-09-05 19:47:51 +0545

    Left side dummy link clean up

--- resources/views/component/front/sidebar.blade.php
+++ resources/views/component/front/sidebar.blade.php
@@ -1,14 +1,7 @@
 <div id="sidebar" class="dropdown-menu" aria-labelledby="sidebar">
     <ul class="list-group">
         <li class="list-group-item">
-            <a href="#" class="nav-link">Cras justo odio</a>
-            <div class="submenu d-none">
-                this is fine
-            </div>
+            <a href="{{url('/')}}" class="nav-link">Home</a>
         </li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Morbi leo risus</li>
-        <li class="list-group-item">Porta ac consectetur ac</li>
-        <li class="list-group-item">Vestibulum at eros</li>
     </ul>
 </div>
