ae1938abfd3aa3fcab0a47f4df4ee085429bc1be
bedus-creation <tmgbedu@gmail.com>
2018-09-08 23:25:16 +0545

    meta tags added for home page

--- resources/views/layouts/app-front.blade.php
+++ resources/views/layouts/app-front.blade.php
@@ -3,9 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
         <title>@yield('title','Sahuba | Online Shopping In Nepal')</title>
+        <meta http-equiv="Lang" content="en" />
+	    <meta name="keywords" content="Online Shopping in nepal, nepalese Online Shop market, Sell your product online, buy products online, sell products online, Nepali bazzar, Online Bazzar,deal in nepal,secondhand products buy and sell, online deal in nepal,kathmandu" />
+	    <meta name="author" content="Sahuba.com offers to sell your products / buy your products online  " />
+	    <meta name="description" content="Online Shopping in Nepal at Sahuba.com - An online Shopping platform for Shopkeeper who want to sell their products throug online.  The common Online Shopping for shop keepers and Customers." />
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
