41d1e6c142602bc15e531447ad01721ad6f649c3
bedus-creation <tmgbedu@gmail.com>
2018-11-19 09:23:28 +0545

    Api tested for products and categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,6 @@ class Category extends Model
     protected $fillable=["name"];
 
     public function products(){
-        return $this->hasMany(Product::class);
+        return $this->hasMany(Product::class)->orderBy('id','desc');
     }
 }
--- tests/Unit/Api/ProductTest.php
+++ tests/Unit/Api/ProductTest.php
@@ -11,9 +11,23 @@ class ProductTest extends TestCase
     use RefreshDatabase; 
 
     /** @test */
-    public function testExample()
+    public function response_has_products()
     {
-        $this->get("graphql?query={products{id,name,price,images{url}}}")
-            ->assertJsonMissing(["errors"]);
+        $this->json("GET","graphql?query={products{id,name,price,images{url}}}")
+            ->assertJsonStructure([
+                'data'=>[
+                    'products'
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function categories_response_to_list_of_products(){
+        $this->json("GET","graphql?query={categories{name,products{id,name,price,images{url}}}}")
+            ->assertJsonStructure([
+                'data'=>[
+                    'categories'
+                ]
+            ]);
     }
 }
