de6ddb3cc532d28f7481298eb522f8161ef25aee
bedus-creation <tmgbedu@gmail.com>
2018-11-24 16:22:11 +0545

    Seems like email template ready

--- resources/views/utils/mail/test.blade.php
+++ resources/views/utils/mail/test.blade.php
@@ -1,38 +1,22 @@
 @component('mail::message')
 
-# Preface 
+# Hello, 
 
-In the few past year, the market of online business has been grown up exponentially. 
-We believe that the reason behind is large number of products, with variety of quality,
-brands, and price etc. So why not participate into the online world to grow your
-business.
+Do you wanna sell new products of your shop ? or even Second-Hand ? If so we are here to Help You.
 
+@component('mail::button', ['url' => url('/'), 'color' => 'success'])
+Sign Up
+@endcomponent
 
-# Introduction
+# Features
 
-Sahuba is Online Shopping portal begin with idea of providing easy products sells 
-for Sahuba’s users. It’s a public platform, where anyone can go to sahuba.com sign-up,
-and sell their products on online for free. There is no limit of number of products
-and also no limit in the numbers of years of service being used. It’s free for forever.
-
-# Features of sahuba.com
-
-- Everyone can sign-up into sahuba.com, by simply providing Shop name, email 
-address and password, there is more easy sign-up or login method available here 
-that is social-network login. There is Facebook or google login option method 
-available.
-- Can add any numbers of products.
-- Have their shop profile with cover and profile picture photo.
-- shop info displays with their products, if someone is interested in their 
-products, they can directly contact you.
-- Comment section for products in-query, and comment will be informed to 
-respected user’s email address. 
+- Buy or Sell products
+- No products Limits
+- Shop Profile.
+- Comment section
+- It's Free, Forever.
 
 
-# We are looking for partnership
-
-We believe that the shopkeeper should be busy on selling their products rather 
-then marketing of their products. 
 
 Thanks,<br>
 {{ config('app.name') }}
